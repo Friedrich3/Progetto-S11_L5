28038ef9556e147aaa4b11c0bb0f59f46f5ba0ba
Federico Tonti <148573975+Friedrich3@users.noreply.github.com>
2025-01-24 18:04:54 +0100

    musicPLayer large

--- src/components/MusicPlayer.tsx
+++ src/components/MusicPlayer.tsx
@@ -2,79 +2,78 @@ import { useEffect, useState } from "react";
 import { Button, Col, Container, Row } from "react-bootstrap";
 import {
   FastForwardFill,
-  PauseCircleFill,
   PauseFill,
-  PlayCircleFill,
   PlayFill,
   RewindFill,
 } from "react-bootstrap-icons";
 import { SearchResponse } from "../types/SearchFetch";
 
-
-
-interface EachSong{
-    song: SearchResponse | null
+interface EachSong {
+  song: SearchResponse | null;
 }
 
-
-const MusicPlayer = function (props :EachSong ) {
+const MusicPlayer = function (props: EachSong) {
   const [isPlaying, setIsPlaying] = useState(false);
-  
-  const [currentAudio,setCurrentAudio] = useState<HTMLAudioElement | null>( null)
 
+  const [currentAudio, setCurrentAudio] = useState<HTMLAudioElement | null>(
+    null
+  );
 
+  useEffect(() => {
+    currentAudio?.pause();
+    setIsPlaying(false);
+    const audio = new Audio(props.song?.preview);
+    setCurrentAudio(audio);
+    return () => {
+      audio.pause();
+      setCurrentAudio(null);
+    };
 
-
-  useEffect(()=>{
-        currentAudio?.pause()
-        setIsPlaying(false)
-        const audio = new Audio(props.song?.preview)
-        setCurrentAudio(audio)
-        return () => {audio.pause()
-            setCurrentAudio(null)
-        }
-       
-       //audio.src = props.song?.preview
-       //audio.src = props.song!.preview
-  },[props.song])
+    //audio.src = props.song?.preview
+    //audio.src = props.song!.preview
+  }, [props.song]);
 
   const playMusic = function () {
-   // audio.src = 'newsong.mp3' SERVE PER CAMBIARE CANZONE
-   if(isPlaying){
-       currentAudio?.pause()
-    }else{
-        currentAudio?.play()
+    // audio.src = 'newsong.mp3' SERVE PER CAMBIARE CANZONE
+    if (isPlaying) {
+      currentAudio?.pause();
+    } else {
+      currentAudio?.play();
     }
     setIsPlaying(!isPlaying);
-    
   };
 
   return (
     <>
-    
       <Container fluid className="custom-position">
-        <Row className="d-none d-lg-flex py-2">
-          <Col className="d-flex justify-content-evenly align-items-center">
-            <RewindFill className=" fs-3" />
-            {isPlaying ? (
-              <PauseCircleFill onClick={playMusic} className=" fs-3" />
-            ) : (
-              <PlayCircleFill onClick={playMusic} className=" fs-3" />
-            )}
-            <FastForwardFill className=" fs-3" />
+        <Row className="d-none d-lg-flex py-2 row-cols-3">
+          <Col className="d-flex justify-content-start align-items-center ">
+            <div className="d-flex">
+              <img
+                src={props.song?.album.cover_small}
+                alt="Cover Canzone"
+                height="50px"
+              />
+              <div className="ps-2">
+                <p className="m-0">{props.song?.title_short}</p>
+                <p className="m-0">{props.song?.artist.name}</p>
+              </div>
+            </div>
+            <div>
+              <RewindFill className=" fs-4" />
+              {isPlaying ? (
+                <PauseFill onClick={playMusic} className=" fs-1 me-3 ms-3" />
+              ) : (
+                <PlayFill onClick={playMusic} className=" fs-1 me-3 ms-3" />
+              )}
+              <FastForwardFill className=" fs-4" />
+            </div>
           </Col>
 
-          <Col className="justify-content-center bg-secondary d-flex align-content-center py-1">
-            {/* {currentSong.length === 0 ? ( */}
-              <img src="../public/assets/logos/apple.svg" alt="Logo Apple" />
-            {/*  ) : (
-              <div className="d-flex ">
-                 <img src="" alt="" />
-                <p>Titolo Canzone</p>
-               </div>
-             )} */}
+          <Col className="justify-content-center bg-secondary d-flex align-content-center py-1 col-3">
+            <img src="../public/assets/logos/apple.svg" alt="Logo Apple" />
           </Col>
-          <Col className=" text-end">
+          <Col className="d-flex justify-content-end pe-4 col-3 ">
             <Button type="button" variant="outline-secondary">
               Accedi
             </Button>
@@ -82,23 +81,22 @@ const MusicPlayer = function (props :EachSong ) {
         </Row>
 
         <Row className="d-flex d-lg-none py-2">
-        <Col className="d-flex justify-content-between align-items-center">
+          <Col className="d-flex justify-content-between align-items-center ">
             <div className="d-flex">
-                <img src={props.song?.album.cover_small} alt="" />
-                <div className="ps-2">
-
+              <img src={props.song?.album.cover_small} alt="" />
+              <div className="ps-2">
                 <p className="m-0">{props.song?.title_short}</p>
                 <p className="m-0">{props.song?.artist.name}</p>
-                </div>
+              </div>
             </div>
             <div>
-            <RewindFill className=" fs-4" />
-            {isPlaying ? (
+              <RewindFill className=" fs-4" />
+              {isPlaying ? (
                 <PauseFill onClick={playMusic} className=" fs-1 me-3 ms-3" />
-            ) : (
+              ) : (
                 <PlayFill onClick={playMusic} className=" fs-1 me-3 ms-3" />
-            )}
-            <FastForwardFill className=" fs-4" />
+              )}
+              <FastForwardFill className=" fs-4" />
             </div>
           </Col>
         </Row>
